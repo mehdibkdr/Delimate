41aa9f4297298d47aedd9896dc90445b7097c54f
mehdibkdr <mn.benabdelkader@gmail.com>
2025-06-27 05:21:39 +0100

    Ajout du domaine Azure dans ALLOWED_HOSTS

--- Delimate/settings.py
+++ Delimate/settings.py
@@ -133,4 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom User Model
 AUTH_USER_MODEL = 'webapp.User'
 
-ALLOWED_HOSTS = ['delimate-webapp.azurewebsites.net']
+ALLOWED_HOSTS = ['delimate-hheffye6eggebefj.canadacentral-01.azurewebsites.net', 'localhost', '127.0.0.1']
+
